100f560b8ce9253fbd81339967e735563bd8e512
Richard <32272387+lauyh@users.noreply.github.com>
2022-10-07 23:47:03 +0800

    fix symbol issue

--- src/db/db.js
+++ src/db/db.js
@@ -1,12 +1,12 @@
-const knex = require('knex')
-require('dotenv').config()
+const knex = require("knex");
+require("dotenv").config();
 
 module.exports = knex({
-	client: 'postgres',
-	connection: {
-		host: process.env.PG_HOST,
-		user: process.env,PG_USERNAME,
-		password: process.env.PG_PASSWORD,
-		database: process.env.DATABASE
-	}
-})
+  client: "postgres",
+  connection: {
+    host: process.env.PG_HOST,
+    user: process.env.PG_USERNAME,
+    password: process.env.PG_PASSWORD,
+    database: process.env.DATABASE,
+  },
+});
