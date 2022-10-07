37686251e7c8bd2f882881d5ff5315923d0f5fa8
Richard <32272387+lauyh@users.noreply.github.com>
2022-10-07 12:29:02 +0800

    add knex migration file and migration script

---
+++ migrations/20221006151902_merchants.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('merchants', function(table) {
+    table.primary(['id', 'merchant_name'])
+    table.increments('id');
+    table.string('merchant_name').notNullable();
+    table.string('phone_number').notNullable();
+    table.decimal('latitude').notNullable();
+    table.decimal('longtitude').notNullable();
+    table.boolean('is_active').notNullable();
+    table.datetime('created_at', { precision: 6 }).defaultTo(knex.fn.now());
+    table.index(['id', 'merchant_name'], 'idx_id_merchant_name')
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('merchants');
+};
