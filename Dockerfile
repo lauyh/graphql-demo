9afab6baf6ffd225af90a77d3b65c9dbdfc262f5
Richard <32272387+lauyh@users.noreply.github.com>
2022-10-11 01:10:53 +0800

    finalize the dev for using docker-compose

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM alpine
 RUN apk add --update nodejs npm
 WORKDIR /var/www/html
 COPY package.json ./
-RUN npm install
+RUN npm install --production
 COPY . .
 CMD ["npm", "start"]
--- src/db/db.js
+++ src/db/db.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 module.exports = knex({
   client: "postgres",
   connection: {
-    host: process.env.PG_HOST,
+    host: process.env.HOST,
     user: process.env.PG_USERNAME,
     password: process.env.PG_PASSWORD,
     database: process.env.DATABASE,
