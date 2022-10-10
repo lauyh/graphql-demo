2ae21b245641c6635dc9b07e3bdbd8af1affe132
Richard <32272387+lauyh@users.noreply.github.com>
2022-10-10 10:32:52 +0800

    update migration script

--- migrations/20221006151902_merchants.js
+++ migrations/20221006151902_merchants.js
@@ -1,25 +1,34 @@
+const moment = require("moment");
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  return knex.schema.createTable('merchants', function(table) {
-    table.primary(['id', 'merchant_name'])
-    table.increments('id');
-    table.string('merchant_name').notNullable();
-    table.string('phone_number').notNullable();
-    table.decimal('latitude').notNullable();
-    table.decimal('longtitude').notNullable();
-    table.boolean('is_active').notNullable();
-    table.datetime('created_at', { precision: 6 }).defaultTo(knex.fn.now());
-    table.index(['id', 'merchant_name', 'is_active'], 'idx_id_merchant_name_is_active')
-  })
+exports.up = function (knex) {
+  return knex.schema.createTable("merchants", function (table) {
+    table.primary(["id", "merchant_name"]);
+    table.increments("id");
+    table.string("merchant_name").notNullable();
+    table.string("phone_number").notNullable();
+    table.decimal("latitude").notNullable();
+    table.decimal("longtitude").notNullable();
+    table.boolean("is_active").notNullable();
+    table
+      .datetime("created_at")
+      .defaultTo(moment(new Date()).format("DD-MM-YYYY HH:mm:ss"));
+    table.index(
+      ["id", "merchant_name", "is_active"],
+      "idx_id_merchant_name_is_active"
+    );
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  return knex.schema.dropTable('merchants');
+exports.down = function (knex) {
+  return knex.schema.dropTable("merchants");
 };
+
+// table.string("created_at").defaultTo(new Date().toISOString());
